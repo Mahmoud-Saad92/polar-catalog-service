c8f7ae626f482b76cf0fed5d3a22782e1cb571c5
Mahmoud Saad <62609750+Mahmoud-Saad92@users.noreply.github.com>
2025-09-23 17:27:31 +0300

    add home controller

---
+++ src/main/java/com/bazinga/eg/catalogservice/resource/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.bazinga.eg.catalogservice.resource.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/catalog")
+public record HomeController() {
+
+    @GetMapping("/")
+    public String getGreeting() {
+        log.info("Received request for greeting");
+
+        return "Welcome to books catalog!";
+    }
+}
